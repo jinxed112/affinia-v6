b2fae174a168552e68143aa0afd8dd84b9d46c52
jinxed112 <michele.terrana@gmail.com>
2025-08-09 11:44:48 +0200

    fix: Update pages index.ts - Replace MirrorRequestsPage with RequestsPage

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -3,9 +3,8 @@ export { HomePage } from './HomePage';
 export { ProfilePage } from './ProfilePage';
 export { MirrorPage } from './MirrorPage';
 export { DiscoveryPage } from './DiscoveryPage';
-export { MirrorRequestsPage } from './MirrorRequestsPage';
+export { RequestsPage } from './RequestsPage';
 export { AdminPage } from './AdminPage';
 export { default as ResetPasswordPage } from './ResetPasswordPage';
-
 // SimpleQuestionnairePage est importé directement dans App.tsx
 // Login est importé directement dans App.tsx
